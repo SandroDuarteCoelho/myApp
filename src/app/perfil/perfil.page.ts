120d56df4f8996df40645c7fe4ad64467aaac9a2
Sandro Coelho <sandroduartecoelho@gmail.com>
2026-05-15 15:22:20 +0100

    50% perfil

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,20 +1,155 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonButtons,
+  IonPopover,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonIcon,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonSearchbar,
+  IonSelect,
+  IonSelectOption,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from '@ionic/angular/standalone';
+
+export type Pessoa = {
+  nome: string;
+  apelido: string;
+  localidade: string;
+  grupo: string;
+  eneagrama_tipo: number | string;
+  data: string;
+};
+
+// (removido tipo duplicado que interrompia o decorator @Component)
+
+
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons],
+
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonButtons,
+
+    IonPopover,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonIcon,
+
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+
+    IonSearchbar,
+    IonSelect,
+    IonSelectOption,
+
+    IonGrid,
+    IonRow,
+    IonCol
+  ],
 })
+
+
+
+
 export class PerfilPage implements OnInit {
 
-  ngOnInit() {}
+
+  primeiroNome = '';
+  ultimoNome = '';
+  dataNascimento = '';
+  localidade = '';
+  grupo = '';
+  eneagrama = '';
+
+  // Lista (pessoas.json)
+  people: Pessoa[] = [];
+  searchText = '';
+  selectedGroup = '';
+  grupos: string[] = [];
+
+  constructor(private readonly http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Pessoa[]>('assets/data/pessoas.json').subscribe({
+      next: (data) => {
+        this.people = data;
+
+        // grupos únicos
+        this.grupos = [...new Set(data.map((p) => p.grupo))];
+      },
+      error: (err) => {
+        console.error('Erro ao carregar pessoas.json', err);
+      },
+    });
+  }
+
 
   voltar() {
     window.location.href = '/oracao-perdao';
   }
+
+  guardar() {
+
+    console.log({
+       primeiroNome: this.primeiroNome,
+    ultimoNome: this.ultimoNome,
+    dataNascimento: this.dataNascimento,
+    localidade: this.localidade,
+    eneagrama: this.eneagrama,
+    grupo: this.grupo
+    });
+
+  }
+
+  filteredPeople(): Pessoa[] {
+    const search = this.searchText.trim().toLowerCase();
+
+    return this.people.filter((p) => {
+      const matchesGroup = !this.selectedGroup || p.grupo === this.selectedGroup;
+
+      const matchesSearch =
+        !search ||
+        (p.nome ?? '').toLowerCase().includes(search) ||
+        (p.apelido ?? '').toLowerCase().includes(search);
+
+      return matchesGroup && matchesSearch;
+    });
+  }
+
 }
 

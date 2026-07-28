9540062a3d8bc9da59bb9bf5b97f0601b42893d5
Sandro Coelho <sandroduartecoelho@gmail.com>
2026-07-28 23:25:51 +0100

    numero residencias

--- src/app/casa-porta/casa-porta.page.ts
+++ src/app/casa-porta/casa-porta.page.ts
@@ -1,57 +1,220 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+
 import { FormsModule } from '@angular/forms';
+
+import { HttpClient } from '@angular/common/http';
 
 
 import {
+
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
   IonButton,
   IonButtons,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardContent,
   IonItem,
   IonLabel,
-  IonInput,
+  IonAccordion,
+  IonAccordionGroup
+
 } from '@ionic/angular/standalone';
 
-@Component({
-  selector: 'app-casa-porta',
-  templateUrl: './casa-porta.page.html',
-  styleUrls: ['./casa-porta.page.scss'],
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonButton,
-    IonButtons,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardContent,
-    IonItem,
-    IonLabel,
-    IonInput,
-  ],
-})
-export class CasaPortaPage {
- 
 
 
-  constructor() {}
 
-  voltar(): void {
-    window.history.back();
-  }
+
+interface Residencia {
+
+
+  numero:number;
+
+
+  titulo:string;
+
+
+  descricao:string;
 
 
 }
 
+
+
+
+
+
+
+@Component({
+
+
+  selector:'app-casa-porta',
+
+
+  templateUrl:'./casa-porta.page.html',
+
+
+  styleUrls:['./casa-porta.page.scss'],
+
+
+  standalone:true,
+
+
+
+  imports:[
+
+
+    CommonModule,
+
+
+    FormsModule,
+
+
+    IonHeader,
+
+    IonToolbar,
+
+    IonTitle,
+
+    IonContent,
+
+    IonButton,
+
+    IonButtons,
+
+    IonItem,
+
+    IonLabel,
+
+    IonAccordion,
+
+    IonAccordionGroup
+
+
+  ]
+
+
+})
+
+
+
+export class CasaPortaPage implements OnInit {
+
+
+
+
+
+  residencias:Residencia[] = [];
+
+
+
+
+
+  constructor(
+
+    private http:HttpClient
+
+  ){}
+
+
+
+
+
+  ngOnInit(){
+
+
+    this.carregarResidencias();
+
+
+  }
+
+
+
+
+
+
+
+  carregarResidencias(){
+
+
+
+    this.http
+
+    .get<any>('assets/data/residencias.json')
+
+
+    .subscribe({
+
+
+
+
+      next:(dados)=>{
+
+
+
+        console.log(
+
+          'JSON residencias:',
+
+          dados
+
+        );
+
+
+
+
+        this.residencias =
+
+          dados.residencias;
+
+
+
+      },
+
+
+
+
+
+      error:(erro)=>{
+
+
+        console.error(
+
+          'Erro ao carregar residencias:',
+
+          erro
+
+        );
+
+
+
+      }
+
+
+
+    });
+
+
+
+  }
+
+
+
+
+
+
+
+  voltar(){
+
+
+    window.history.back();
+
+
+  }
+
+
+
+
+
+}
